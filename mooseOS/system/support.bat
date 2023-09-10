3e00f4e5be2df99ff4b199904d6d87f5e24d4d7f
TrisoMorp <144601122+TrisoMorp@users.noreply.github.com>
2023-09-10 15:57:21 +0100

    Create support.bat

---
+++ mooseOS/system/support.bat
@@ -0,0 +1,12 @@
+@echo off
+goto app.moose.support
+:app.moose.support
+title mooseOS Support. 
+echo Welcome (and thank you) for using mooseOS. 
+echo.
+echo System:
+echo System Date: %date%
+echo System Time: %time%
+echo System Username (this will not be your mooseOS username): %username%
+echo.
+echo (C) Moose - MMXXIII.
